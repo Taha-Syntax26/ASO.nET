3fbbdcbcea346f61a2a76e7457a04757e9bdff4b
asp <asp@aptech.com>
2024-11-04 12:47:55 +0500

    1st

---
+++ WebApplicationtaha/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationtaha.Models;
+
+namespace WebApplicationtaha.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
